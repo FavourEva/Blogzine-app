66a6e5e9b90a57e382628eeec31ecdee59e48788
Onyeocha Favour <97852822+FavourEva@users.noreply.github.com>
2023-03-22 21:46:48 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -70,17 +70,17 @@ module "eks" {
     one = {
       name = "node-group-1"
 
-      instance_types = ["t3.medium"]
+      instance_types = ["t2.medium"]
 
       min_size     = 1
       max_size     = 3
-      desired_size = 2
+      desired_size = 1
     }
 
     two = {
       name = "node-group-2"
 
-      instance_types = ["t3.medium"]
+      instance_types = ["t2.medium"]
 
       min_size     = 1
       max_size     = 2
